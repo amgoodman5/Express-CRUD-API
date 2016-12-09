189c154f3fa73d2ebd162af44ab1420d37718065
Aaron Goodman <Amgoodman5@Aarons-MacBook-Air.local>
2016-12-08 17:33:09 -0700

    seed update

--- seeds/14er.js
+++ seeds/14er.js
@@ -4,21 +4,21 @@ exports.seed = function(knex, Promise) {
                 const mountains = [{
                             name: 'Crestone',
                             altitude: 14294,
-                            text: 'Hard',
+                            difficulty: 'Hard',
                         },
                         {
                         name: 'Crestone Needle',
                         altitude: 14197,
-                        text: 'Hard AF',
+                        difficulty: 'Hard AF',
                     },
                     {
                     name: 'Kit Carson',
                     altitude: 14165,
-                    text: 'Basic',
+                    difficulty: 'Basic',
             },{
             name: 'Blanca Peak',
             altitude: 14345,
-            text: 'Basic',
+            difficulty: 'Basic',
         }];
 
 
